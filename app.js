dfd4c6316cbec52b0c006be3677f40d9c8cad078
Spencer Bartlett <senteo1@yahoo.com>
2020-03-16 14:47:51 -0400

    move export to end of file

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-module.exports = server
-
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
@@ -72,3 +70,5 @@ app.post("/api", function(req, res) {
 const listener = app.listen(process.env.PORT || 8070, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+module.exports = app.listen(8070);
