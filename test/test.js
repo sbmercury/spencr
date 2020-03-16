7fbc448c7bcae40c0bf75219777d7cfa3e683c25
Spencer Bartlett <senteo1@yahoo.com>
2020-03-16 14:03:34 -0400

    add testing

---
+++ test/test.js
@@ -0,0 +1,15 @@
+var request = require('supertest');
+ 
+var app = require('../app.js');
+ 
+ 
+ 
+describe('GET /', function() {
+ 
+  it('check for 200 reponse code', function(done) {
+ 
+    request(app).get('/').expect(200, done);
+ 
+  });
+ 
+});
